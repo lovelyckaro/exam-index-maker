73e3e9c83b2c11758e11da2dbf483aedf31ee625
Love Lyckaro <love.lyckaro@hotmail.se>
2021-01-12 19:53:15 +0100

    Add comments to shell scripts

--- src/combine.sh
+++ src/combine.sh
@@ -1,10 +1,11 @@
+# For directory in tmp (set of exam + solutions)
 for d in tmp/*/ ; do
     tmp=${d%%/}
-    texname=${tmp##*/}i.tex
-    pdfname=${tmp##*/}i.pdf
-    ./generateLatex $d $texname
-    latexmk -pdf -silent -outdir=trash $texname
-    rm $texname
-    mv trash/$pdfname exams/
-    rm -rf trash
+    texname=${tmp##*/}i.tex # filename for output tex file
+    pdfname=${tmp##*/}i.pdf # filename for output pdf file
+    ./generateLatex $d $texname # generateLatex for combining the files
+    latexmk -pdf -silent -outdir=trash $texname # Compile the latex into a pdf
+    rm $texname # remove the generated latex file
+    mv trash/$pdfname exams/ # Move the pdf file to exams/
+    rm -rf trash # Remove aux latex files
 done
--- src/download.sh
+++ src/download.sh
@@ -1,17 +1,18 @@
 base="https://chalmerstenta.se"
 end="/test"
 
+# Paths are all links inside a td element on the requested page
 paths=$( curl -# $1 | pup "td a attr{href}")
 for path in $paths
 do
-    if [[ $path == /tenta/* ]]
+    if [[ $path == /tenta/* ]] # If the path begins with /tenta/ we want it
     then
-        if [[ $path == *i.pdf ]]
-        then
+        if [[ $path == *i.pdf ]] # If path ends with i.pdf the solutions and exam are in the same pdf
+        then # Download into exams directly
             cd exams
             curl -O -J -# $base$path
             cd ../ 
-        else
+        else # Otherwise we need to download the exam and solution pdfs separately and combine later
             cd tmp
             fn=${path##*/}
             dn=${fn%?.pdf}
