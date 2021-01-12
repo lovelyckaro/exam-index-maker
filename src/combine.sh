8815793c9379de700028e01865c2c2a05aae0305
Love Lyckaro <love.lyckaro@hotmail.se>
2021-01-12 22:12:30 +0100

    Supress output from latexmk

--- src/combine.sh
+++ src/combine.sh
@@ -4,7 +4,7 @@ for d in tmp/*/ ; do
     texname=${tmp##*/}i.tex # filename for output tex file
     pdfname=${tmp##*/}i.pdf # filename for output pdf file
     ./generateLatex $d $texname # generateLatex for combining the files
-    latexmk -pdf -silent -outdir=trash $texname # Compile the latex into a pdf
+    latexmk -pdf -silent -outdir=trash $texname > /dev/null # Compile the latex into a pdf
     rm $texname # remove the generated latex file
     mv trash/$pdfname exams/ # Move the pdf file to exams/
     rm -rf trash # Remove aux latex files
